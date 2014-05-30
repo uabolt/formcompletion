12e84215de3714e00a05a48f2509e4d1eb804014
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2014-05-29 20:55:40 -0700

    added EnabledImageManager

--- formcompletion/imagetask/models.py
+++ formcompletion/imagetask/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
+class EnabledImageManager(models.Manager):
+    def get_queryset(self):
+        return super(EnabledImageManager, self).get_queryset().filter(enabled=True)
+
 class Image(models.Model):
     title = models.CharField(max_length=50)
     file = models.FileField(upload_to='images')
     enabled = models.BooleanField(default=True)
+
+    objects = models.Manager()
+    enabled_objects = EnabledImageManager()
 
     def __unicode__(self):
         return self.title
