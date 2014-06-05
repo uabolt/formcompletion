fad5935c24d05a41a42e9a26e19776d8e74d7940
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2014-06-04 18:23:35 -0700

    added teacher view

--- formcompletion/imagetask/urls.py
+++ formcompletion/imagetask/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('imagetask.views',
         url(r'^student/$', 'student', name='imagetask_student'),
+        url(r'^teacher/$', 'teacher', name='imagetask_teacher'),
 )
--- formcompletion/imagetask/views.py
+++ formcompletion/imagetask/views.py
@@ -5,3 +5,8 @@ def student(request, template_name='imagetask/student.html'):
     images = Image.enabled_objects.all()
     data = dict(images=images)
     return render(request, template_name, data)
+
+def teacher(request, template_name='imagetask/teacher.html'):
+    images = Image.enabled_objects.order_by('?')[:3]
+    data = dict(images=images)
+    return render(request, template_name, data)
