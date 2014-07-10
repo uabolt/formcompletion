5bb1ffb4395243cfc38afbdbc2daeaa35a78f97a
enoriega <enoriega@sumukan.sista.arizona.edu>
2014-07-09 20:56:02 -0700

    Commented the sites framework in order to run without error in other deployments

--- formcompletion/formcompletion/settings.py
+++ formcompletion/formcompletion/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'django.contrib.admin',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
